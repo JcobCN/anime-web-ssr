77e9e7c6eac81c741cd5e455bf26ac853cdb244e
t <t@t.com>
2023-02-16 08:38:38 +0800

    add build target as es2015

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import ssr from 'vite-plugin-ssr/plugin'
 
+/** @type {import('vite').UserConfig} */
 export default {
-  plugins: [vue(), ssr()]
+  plugins: [vue(), ssr()],
+  build: {
+    target: 'es2015'
+  }
 }
